9730bb3ac1800def23949c5bc941000c67d199a0
Mark Mayer <mdm373@gmail.com>
2020-01-09 04:58:00 +0000

    simplify map state

--- src/app/global-content/map/map.tsx
+++ src/app/global-content/map/map.tsx
@@ -7,29 +7,23 @@ import Helmet from "react-helmet";
 export type MapLoadHandler = (map: google.maps.Map) => void
 
 export const AppMap: React.FC<{mapId: string, onMapLoad: MapLoadHandler}> = (props) => {
-    const [mapsUrl, setMapsUrl] = React.useState("")
-    React.useEffect(() => {
-        (async()=>{
-            const config = getAppConfig()
-            setMapsUrl(`https://maps.googleapis.com/maps/api/js?key=${config.mapsApiKey}&callback=initMap`)
-            const initMap = () => {
-                const mapElement = document.getElementById(props.mapId)
-                if(!mapElement) {
-                    throw new Error(`could not find map element '${props.mapId}'`)
-                }
-                const mapConfig = {
-                    center: { lat:40.730610, lng: -73.935242},
-                    zoom: 10,
-                };
-                props.onMapLoad(new google.maps.Map(mapElement, mapConfig));
-            };
-            (window as any).initMap = initMap;
-        })()
-    })
-    return <div className="h-100 w-100">
-        <div id={props.mapId} className="h-100 w-100"></div>
+    const config = getAppConfig()
+    const initMap = () => {
+        const mapElement = document.getElementById(props.mapId)
+        if(!mapElement) {
+            throw new Error(`could not find map element '${props.mapId}'`)
+        }
+        const mapConfig = {
+            center: { lat:40.730610, lng: -73.935242},
+            zoom: 10,
+        };
+        props.onMapLoad(new google.maps.Map(mapElement, mapConfig));
+    };
+    (window as any).initMap = initMap;
+    return <div id={props.mapId} className="h-100 w-100">
+        <div  className="h-100 w-100"></div>
         <Helmet>
-            <script src={mapsUrl} async defer></script>
+            <script src={`https://maps.googleapis.com/maps/api/js?key=${config.mapsApiKey}&callback=initMap`} async defer></script>
         </Helmet>
     </div>
  }
